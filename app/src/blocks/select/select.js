4973c8a16e58114bb647ddd0dbea5bad0b81b8da
R45H <gmr45h@gmail.com>
2018-08-01 10:34:05 +0300

    Add simple version of the select

--- app/src/blocks/select/select.js
+++ app/src/blocks/select/select.js
@@ -1,23 +1,34 @@
 var
 	classBlock = 'select',
+	classSimple = classBlock + '_simple',
 	$select = $('.' + classBlock + '__origin');
 
-$select.selectmenu({
-	classes: {
-		'ui-selectmenu-button': classBlock + '__current', // Кнопка селекта
-		'ui-selectmenu-button-open': classBlock + '__current_active', // Состояние кнопки при открытом селекте
-		'ui-selectmenu-text': classBlock + '__text', // Текст внутри кнопки
-		'ui-selectmenu-menu': classBlock + '__list' // Выпадающий список
-	},
-	create: function() {
-		$(this)
-			.next()
-			.append('' +
-				'<svg class="' + classBlock + '__icon">' +
-					'<use xlink:href="img/sprite.svg#svg-arrow-down"></use>' +
-				'</svg>'
-			);
+$select.each(function() {
+	var
+		$this = $(this),
+		classList = classBlock + '__list';
+
+	if ($this.closest('.' + classBlock).hasClass(classSimple)) {
+		classList += ' ' + classList + '_simple';
 	}
+
+	$this.selectmenu({
+		classes: {
+			'ui-selectmenu-button': classBlock + '__current', // Кнопка селекта
+			'ui-selectmenu-button-open': classBlock + '__current_active', // Состояние кнопки при открытом селекте
+			'ui-selectmenu-text': classBlock + '__text', // Текст внутри кнопки
+			'ui-selectmenu-menu': classList // Выпадающий список
+		},
+		create: function() {
+			$(this)
+				.next()
+				.append('' +
+					'<svg class="' + classBlock + '__icon">' +
+					'<use xlink:href="img/sprite.svg#svg-arrow-down"></use>' +
+					'</svg>'
+				);
+		}
+	});
 });
 
 $(window).on('resize', function() {
